dc22024023564d07f5f0ecc0177ffdbd0105e004
Segu <delaquintana95119@sistemas.frc.utn.edu.ar>
2024-12-23 14:53:30 -0300

    customItemPl

--- src/main/java/org/cowCannon/cowCannon/CustomItem.java
+++ src/main/java/org/cowCannon/cowCannon/CustomItem.java
@@ -1,16 +1,43 @@
 package org.cowCannon.cowCannon;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class CustomItem implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label,
 			@NotNull String[] args) {
-		// TODO Auto-generated method stub
+		if (!(sender instanceof Player)){
+			sender.sendMessage(ChatColor.RED + "That command only can be executed by a player");
+			return true;
+		}
+
+		Player player = (Player) sender;
+		ItemStack customBucket = new ItemStack(Material.BUCKET);
+		ItemMeta meta = customBucket.getItemMeta();
+		meta.setDisplayName(ChatColor.RED + "Exploder 3000");
+		meta.setLore(Arrays.asList(ChatColor.GREEN + "Make any cow explode!", ChatColor.RED + "Be careful!!"));
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		meta.addEnchant(Enchantment.ARROW_INFINITE, 1, true); //add a decoration to the item
+		meta.getPersistentDataContainer().set(Keys.CUSTOM_BUCKET, PersistentDataType.BOOLEAN, true);
+
+		customBucket.setItemMeta(meta);
+
+		player.getInventory().addItem(customBucket);
 		return false;
 	}
 	
--- src/main/java/org/cowCannon/cowCannon/Keys.java
+++ src/main/java/org/cowCannon/cowCannon/Keys.java
@@ -2,8 +2,8 @@ package org.cowCannon.cowCannon;
 import org.bukkit.NamespacedKey;	
 
 public class Keys {
-	public static final NamespacedKey CUSTOM_DIAMOND = new NamespacedKey(CowCannon.getInstance(),
-			"CustomDiamond"); 
+	public static final NamespacedKey CUSTOM_BUCKET = new NamespacedKey(CowCannon.getInstance(),
+			"CustomBucket");
 	public static final NamespacedKey CUSTOM_COW = new NamespacedKey(CowCannon.getInstance(),
 			"CustomCow"); 
 }
